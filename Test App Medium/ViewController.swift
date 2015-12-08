ce7f021c7a6eb5f18cb1013dbef140865dc552c0
Gustavo Parreira <gustashminecraft@gmail.com>
2015-12-08 17:19:25 +0000

    Initial Commit

---
+++ Test App Medium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test App Medium
+//
+//  Created by Gustavo Parreira on 08/12/15.
+//  Copyright © 2015 Professional Betting Tips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
